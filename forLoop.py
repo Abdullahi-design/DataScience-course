cc439990fdaa8fa8adf7b7f73f6879a332a1eec9
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-12-26 08:47:14 +0100

    continue and break statement

--- forLoop.py
+++ forLoop.py
@@ -1,3 +1,19 @@
-for i in range(1, 10, 2):
+# loop between 1 to 10 with an increment of 2 between each iteration
+# for i in range(1, 10, 2):
+#     print(i)
+# print("the loop ends here")
+
+# break statements
+# for i in range(1,10):
+#     if i == 5:
+#         break
+#     print(i)
+# print("the loop ends here")
+
+
+# continue statements
+for i in range(10):
+    if i == 5:
+        continue
     print(i)
 print("the loop ends here")
